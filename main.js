566106075fb84c6419f4c17383031d94dbdf5353
arifeensifat <arifeen.sifat@gmail.com>
2023-04-13 16:46:27 +0600

    sifatshop

---
+++ main.js
@@ -0,0 +1,56 @@
+// js for toggle menu 
+var MenuItems =document.getElementById("MenuItems");
+MenuItems.style.maxHeight = "0px";
+function menutoggle(){
+    if(MenuItems.style.maxHeight == "0px")
+    {
+        MenuItems.style.maxHeight = "200px";
+
+    }
+    else
+    {
+        MenuItems.style.maxHeight = "0px";
+
+    }
+}
+
+// js for product gallery 
+var ProductImg =document.getElementByID("ProductImg");
+var SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick = function()
+{
+ProductImg.src = SmallImg[0].src;
+}
+
+SmallImg[1].onclick = function()
+{
+ProductImg.src = SmallImg[1].src;
+}
+SmallImg[2].onclick = function()
+{
+ProductImg.src = SmallImg[2].src;
+}
+SmallImg[3].onclick = function()
+{
+ProductImg.src = SmallImg[3].src;
+}
+
+
+//  js for togglr form 
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+     function register(){
+        RegForm.style.transform = "translateX(0px)";
+        LoginForm.style.transform = "translateX(0px)";
+        Indicator.style.transform = "translateX(100px)";
+
+     }
+     function login(){
+        RegForm.style.transform = "translateX(300px)";
+        LoginForm.style.transform = "translateX(300px)";
+        Indicator.style.transform = "translateX(0px)";
+
+     }
